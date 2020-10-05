1e30d0e7bb7155f7c8e3025400564625a70392e0
Sreimer78 <sreimer@hawkiit.edu>
2020-10-05 18:18:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 // Creating an object based off of the users input
 function addUser() 
 {
-	var user = {first_name: "john, last_name: "wayne, age: 30};
+	var user = {first_name, last_name, age};
 	
 	// Using the stringify() method to convert my JS object into a JSON object
-	var myJSON = JSON.stringify(addUser);
+	var myJSON = JSON.stringify(user);
 };
 
 // Display that data from the object
 function displayUser() 
 {
-	document.getElementById("demo").innerHTML = myJSON;
+	var user, x;
+		for (x in user) {
+			document.getElementById("demo").innerHTML += myJSON[x] + "<br>";
+		}
 };
