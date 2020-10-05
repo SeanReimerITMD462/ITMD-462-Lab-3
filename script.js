e7580bd7db782f4c7be925b7c242fbaf86548a0a
Sreimer78 <sreimer@hawkiit.edu>
2020-10-05 17:37:09 -0500

    Update script.js
    adding JSON into javascript file

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Creating an object based off of the users input
-function addUser() {
-	var User = {first_name:"", last_name:"",  age:""};
+function addUser() 
+{
+	var user = {first_name: "john, last_name: "wayne, age: 30};
 	
-	// Display that data from the object
-	document.getElementById("demo").innerHTML = "User Info: " + User.type;
+	// Using the stringify() method to convert my JS object into a JSON object
+	var myJSON = JSON.stringify(addUser);
+};
+
+// Display that data from the object
+function displayUser() 
+{
+	document.getElementById("demo").innerHTML = myJSON;
+};
